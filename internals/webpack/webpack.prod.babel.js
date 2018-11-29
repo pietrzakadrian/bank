f39e7b6c1f38a014a4c60e57e2393f9070b62c1a
PietrzakAdrian <contact.pietrzakadrian@gmail.com>
2018-11-29 22:18:10 +0100

    webpack build fix engine

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -20,6 +20,7 @@ module.exports = require('./webpack.base.babel')({
   output: {
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].chunk.js',
+    publicPath: './',
   },
 
   optimization: {
