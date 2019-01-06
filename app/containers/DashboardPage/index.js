d14caae086bd3a1c3091086c06d05c835a09c123
PietrzakAdrian <contact.pietrzakadrian@gmail.com>
2019-01-06 12:36:28 +0100

    add DashboardPage container GridLayout draggable 0.2

--- app/containers/DashboardPage/index.js
+++ app/containers/DashboardPage/index.js
@@ -13,11 +13,9 @@ import React, { Component, Fragment } from 'react';
 import Helmet from 'react-helmet';
 import { withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
-import Grid from '@material-ui/core/Grid';
 import GridLayout from 'react-grid-layout';
 import withAuth from '../../services/withAuth';
-import '../../../node_modules/react-grid-layout/css/styles.css';
-import '../../../node_modules/react-resizable/css/styles.css';
+import './styles.css';
 
 // import { FormattedMessage } from 'react-intl';
 // import messages from './messages';
@@ -59,6 +57,9 @@ const styles = theme => ({
   informationHeaderLast: {
     textAlign: 'right',
   },
+  gridItem: {
+    cursor: 'move',
+  },
 });
 
 class DashboardPage extends Component {
@@ -77,27 +78,89 @@ class DashboardPage extends Component {
 
         <GreetingHeadline id={this.props.user.id} />
         <div className={classes.container}>
-          <GridLayout className="layout" cols={9} rowHeight={30} width={1100}>
-            <div key="a" data-grid={{ x: 0, y: 0, w: 3, h: 3, static: true }}>
+          <GridLayout
+            className="layout"
+            cols={3}
+            rowHeight={9}
+            width={1100}
+            margin={[20, 10]}
+            isResizable={false}
+          >
+            <div key="a" data-grid={{ x: 0, y: 0, w: 1, h: 6, static: true }}>
               <AvailableFunds id={this.props.user.id} />
             </div>
-            <div key="b" data-grid={{ x: 3, y: 0, w: 3, h: 3, static: true }}>
+            <div key="b" data-grid={{ x: 1, y: 0, w: 1, h: 6, static: true }}>
               <BankInformation />
             </div>
-            <div key="c" data-grid={{ x: 6, y: 0, w: 3, h: 3, static: true }}>
+            <div key="c" data-grid={{ x: 2, y: 0, w: 1, h: 6, static: true }}>
               <BankInformation />
             </div>
+
+            {/* 2 belka */}
+
             <div
               key="d"
-              data-grid={{ x: 0, y: 1, w: 6, h: 7, maxW: 6, minW: 6 }}
+              data-grid={{
+                x: 0,
+                y: 2,
+                w: 2,
+                h: 14,
+              }}
+              className={classes.gridItem}
             >
               <AccountBills id={this.props.user.id} />
             </div>
             <div
               key="e"
-              data-grid={{ x: 6, y: 1, w: 3, h: 7, maxW: 3, minW: 3 }}
+              data-grid={{
+                x: 2,
+                y: 2,
+                w: 1,
+                h: 14,
+              }}
+              className={classes.gridItem}
             >
               <RecentTransactions id={this.props.user.id} />
+            </div>
+
+            {/* 2 belka */}
+
+            <div
+              key="f"
+              data-grid={{
+                x: 0,
+                y: 3,
+                w: 1,
+                h: 14,
+              }}
+              className={classes.gridItem}
+            >
+              <AccountBills id={this.props.user.id} />
+            </div>
+
+            <div
+              key="g"
+              data-grid={{
+                x: 1,
+                y: 3,
+                w: 1,
+                h: 14,
+              }}
+            >
+              <AccountBills id={this.props.user.id} />
+            </div>
+
+            <div
+              key="h"
+              data-grid={{
+                x: 2,
+                y: 2,
+                w: 1,
+                h: 14,
+              }}
+              className={classes.gridItem}
+            >
+              <AccountBills id={this.props.user.id} />
             </div>
           </GridLayout>
 
