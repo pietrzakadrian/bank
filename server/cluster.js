0941e6a6d4fe4c1ff5cdf16fb5ded6a857c30126
PietrzakAdrian <contact.pietrzakadrian@gmail.com>
2019-02-09 10:10:47 +0100

    add cluster fix

---
+++ server/cluster.js
@@ -0,0 +1,24 @@
+// Include the cluster module
+const cluster = require('cluster');
+
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+  const cpuCount = require('os').cpus().length;
+
+  // Create a worker for each CPU
+  for (let i = 0; i <= cpuCount; i += 1) {
+    cluster.fork();
+  }
+
+  // Listen for dying workers
+  cluster.on('exit', worker => {
+    // Replace the dead worker, we're not sentimental
+    console.log('Worker %d died :(', worker.id);
+    cluster.fork();
+  });
+
+  // Code to run if we're in a worker process
+} else {
+  require('./index.js');
+}
