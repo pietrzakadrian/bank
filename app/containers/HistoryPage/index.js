3be1d50afd8476b07a6619090c27de1a4499fde1
PietrzakAdrian <contact.pietrzakadrian@gmail.com>
2019-04-06 17:39:43 +0200

    email theme

--- app/containers/HistoryPage/index.js
+++ app/containers/HistoryPage/index.js
@@ -14,14 +14,6 @@ import { compose } from 'redux';
 
 // Import Material-UI
 import { withStyles } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper';
-import { DataTypeProvider } from '@devexpress/dx-react-grid';
-import {
-  Grid,
-  Table,
-  TableColumnVisibility,
-  TableHeaderRow,
-} from '@devexpress/dx-react-grid-material-ui';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
@@ -30,127 +22,24 @@ import makeSelectHistoryPage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
-import Statistics from './Statistics';
 
-const CurrencyFormatter = ({ value }) => (
-  <span style={{ color: '#ea0000' }}>
-    -
-    {value
-      .toFixed(2)
-      .toString()
-      .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
-      .replace('.', ',')}{' '}
-    USD
-  </span>
-);
-
-const CurrencyTypeProvider = props => (
-  <DataTypeProvider formatterComponent={CurrencyFormatter} {...props} />
-);
-
-const headerCellStyles = theme => ({
-  text: {
-    color: 'black',
-    backgroundColor: '#f7f7f7',
-    fontSize: 17,
-  },
-});
-const HeaderCellBase = ({ classes, className, ...restProps }) => (
-  <TableHeaderRow.Cell
-    {...restProps}
-    className={`${classes.text} ${className}`}
-  />
-);
-
-const TableCellBase = ({ classes, className, ...restProps }) => (
-  <Table.Cell {...restProps} className={`${classes.text} ${className}`} />
-);
-
-const TableCellStyles = theme => ({
-  text: {
-    fontSize: 14.5,
-  },
-  backgroundColor: '#fefefe',
-});
-
-const HeaderCell = withStyles(headerCellStyles, { name: 'HeaderCellBase' })(
-  HeaderCellBase,
-);
-
-const TableCell = withStyles(TableCellStyles, { name: 'TableCellBase' })(
-  TableCellBase,
-);
-
-// Import Styles
-const styles = theme => ({
-  center: {
-    textAlign: 'center',
-    maxWidth: 1100,
-    margin: '0 auto',
-  },
-  elevation2: {
-    boxShadow: 'none',
-  },
-  tableBodyContainer: {
-    backgroundColor: '#fefefe',
-  },
-});
 class HistoryPage extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      columns: [
-        { name: 'date', title: 'Date' },
-        { name: 'sender', title: 'Sender' },
-        { name: 'recipient', title: 'Recipient' },
-        { name: 'transferTitle', title: 'Transfer title' },
-        { name: 'amount', title: 'Amount of money' },
-      ],
-      rows: generateRows({ columnValues: testBank, length: 13 }),
-      currencyColumns: ['amount'],
-      defaultHiddenColumnNames: ['transferTitle', 'date'],
-    };
   }
 
   render() {
-    const {
-      rows,
-      columns,
-      currencyColumns,
-      defaultHiddenColumnNames,
-    } = this.state;
-    const { classes } = this.props;
-
     return (
       <Fragment>
         <FormattedMessage {...messages.helmetHistoryTitle}>
           {title => <Helmet title={title} />}
         </FormattedMessage>
-
-        <div className={classes.center}>
-          <Statistics />
-          <div className={classes.historyContainer}>
-            <Paper classes={{ elevation2: classes.elevation2 }}>
-              <Grid rows={rows} columns={columns}>
-                <Table cellComponent={TableCell} />
-                <TableHeaderRow cellComponent={HeaderCell} />
-                <TableColumnVisibility
-                  defaultHiddenColumnNames={defaultHiddenColumnNames}
-                />
-                <CurrencyTypeProvider for={currencyColumns} />
-              </Grid>
-            </Paper>
-          </div>
-        </div>
       </Fragment>
     );
   }
 }
 
-HistoryPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-};
+HistoryPage.propTypes = {};
 
 const mapStateToProps = createStructuredSelector({
   historyPage: makeSelectHistoryPage(),
@@ -171,7 +60,6 @@ const withReducer = injectReducer({ key: 'historyPage', reducer });
 const withSaga = injectSaga({ key: 'historyPage', saga });
 
 export default compose(
-  withStyles(styles),
   withReducer,
   withSaga,
   withConnect,
--- server/controllers/transaction.controller.js
+++ server/controllers/transaction.controller.js
@@ -291,6 +291,10 @@ exports.register = (req, res) => {
   //   return formattedAmountMoney;
   // }
 
+  function getEmailMessage() {
+    return ``;
+  }
+
   async function sendAuthorizationKey(
     senderId,
     recipientId,
@@ -311,13 +315,210 @@ exports.register = (req, res) => {
     const mailOptions = {
       from: `"Bank Application" <${env.nodemailer.email}>`,
       to: `${await getSenderEmail(senderId)}`,
-      subject: 'Autoryzacja płatności',
-      text: `Drogi kliencie! Zarejestrowaliśmy próbę wykonania płatności na kwotę: ${amountMoney} PLN do ${await getRecipientName(
-        recipientId,
-      )}. Potwierdź płatność, wpisując klucz autoryzacyjny: ${authorizationKey}`,
-      html: `Drogi kliencie!<br>Zarejestrowaliśmy próbę wykonania płatności na kwotę: ${amountMoney} PLN do ${await getRecipientName(
-        recipientId,
-      )}.<br><br>Potwierdź płatność, wpisując klucz autoryzacyjny: <b>${authorizationKey}</b>`,
+      subject: 'Payment authorization',
+      text: `Dear Customer! We have registered an attempt to make a payment for the amount of: ${amountMoney} USD to ${await getRecipientName(recipientId)}.
+      Confirm the payment by entering the authorization key: ${authorizationKey}`,
+      html: `<!doctype html>
+      <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+      
+      <head>
+        <title> </title>
+        <!--[if !mso]><!-- -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!--<![endif]-->
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style type="text/css">
+          #outlook a {
+            padding: 0;
+          }
+      
+          .ReadMsgBody {
+            width: 100%;
+          }
+      
+          .ExternalClass {
+            width: 100%;
+          }
+      
+          .ExternalClass * {
+            line-height: 100%;
+          }
+      
+          body {
+            margin: 0;
+            padding: 0;
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+          }
+      
+          table,
+          td {
+            border-collapse: collapse;
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+          }
+      
+          img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
+            -ms-interpolation-mode: bicubic;
+          }
+      
+          p {
+            display: block;
+            margin: 13px 0;
+          }
+        </style>
+        <!--[if !mso]><!-->
+        <style type="text/css">
+          @media only screen and (max-width:480px) {
+            @-ms-viewport {
+              width: 320px;
+            }
+            @viewport {
+              width: 320px;
+            }
+          }
+        </style>
+        <!--<![endif]-->
+        <!--[if mso]>
+              <xml>
+              <o:OfficeDocumentSettings>
+                <o:AllowPNG/>
+                <o:PixelsPerInch>96</o:PixelsPerInch>
+              </o:OfficeDocumentSettings>
+              </xml>
+              <![endif]-->
+        <!--[if lte mso 11]>
+              <style type="text/css">
+                .outlook-group-fix { width:100% !important; }
+              </style>
+              <![endif]-->
+        <style type="text/css">
+          @media only screen and (min-width:480px) {
+            .mj-column-per-100 {
+              width: 100% !important;
+              max-width: 100%;
+            }
+          }
+        </style>
+        <style type="text/css">
+          @media only screen and (max-width:480px) {
+            table.full-width-mobile {
+              width: 100% !important;
+            }
+            td.full-width-mobile {
+              width: auto !important;
+            }
+          }
+        </style>
+      </head>
+      
+      <body>
+        <div style="">
+          <!--[if mso | IE]>
+            <table
+               align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600"
+            >
+              <tr>
+                <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+            <![endif]-->
+          <div style="Margin:0px auto;max-width:600px;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
+              <tbody>
+                <tr>
+                  <td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;vertical-align:top;">
+                    <!--[if mso | IE]>
+                        <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                      
+              <tr>
+            
+                  <td
+                     class="" style="vertical-align:top;width:600px;"
+                  >
+                <![endif]-->
+                    <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                        <tr>
+                          <td align="center" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
+                              <tbody>
+                                <tr>
+                                  <td style="width:200px;"> <img height="auto" src="https://maxrec.pl/img/logobank.jpg" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;" width="200" /> </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <p style="border-top:solid 4px #15a0dd;font-size:1;margin:0px auto;width:100%;"> </p>
+                            <!--[if mso | IE]>
+              <table
+                 align="center" border="0" cellpadding="0" cellspacing="0" style="border-top:solid 4px #15a0dd;font-size:1;margin:0px auto;width:550px;" role="presentation" width="550px"
+              >
+                <tr>
+                  <td style="height:0;line-height:0;">
+                    &nbsp;
+                  </td>
+                </tr>
+              </table>
+            <![endif]-->
+                          </td>
+                        </tr>
+                        <tr>
+                          <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <div style="font-family:helvetica;font-size:16px;line-height:1;text-align:left;color:black;"> Dear Customer! <br /> We have registered an attempt to make a payment for the amount of ${amountMoney} USD to ${await getRecipientName(recipientId)}. <br /><br /> Confirm the payment by entering the authorization key: <b>${authorizationKey}</b>                        </div>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <p style="border-top:solid 4px #15a0dd;font-size:1;margin:0px auto;width:100%;"> </p>
+                            <!--[if mso | IE]>
+              <table
+                 align="center" border="0" cellpadding="0" cellspacing="0" style="border-top:solid 4px #15a0dd;font-size:1;margin:0px auto;width:550px;" role="presentation" width="550px"
+              >
+                <tr>
+                  <td style="height:0;line-height:0;">
+                    &nbsp;
+                  </td>
+                </tr>
+              </table>
+            <![endif]-->
+                          </td>
+                        </tr>
+                        <tr>
+                          <td align="left" style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                            <div style="font-family:helvetica;font-size:16px;line-height:1;text-align:left;color:black;"> Thank you for using our banking services. </div>
+                          </td>
+                        </tr>
+                      </table>
+                    </div>
+                    <!--[if mso | IE]>
+                  </td>
+                
+              </tr>
+            
+                        </table>
+                      <![endif]-->
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <!--[if mso | IE]>
+                </td>
+              </tr>
+            </table>
+            <![endif]-->
+        </div>
+      </body>
+      
+      </html>`,
     };
 
     await transporter.sendMail(mailOptions);
