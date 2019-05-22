40c1ec5c0806d657bb712c4cf5da41ea4de955f6
api <api@crehler.com>
2019-05-22 16:30:25 +0200

    mysql bugfix login change type

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, Sequelize) => {
         autoIncrement: true,
       },
       login: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.BIGINT,
         allowNull: false,
         isNumeric: true,
         validate: {
