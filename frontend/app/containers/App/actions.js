03589b047e74d89281b45ee833447899e16640e3
api <api@crehler.com>
2019-07-05 16:27:40 +0200

    cleancode

--- frontend/app/containers/App/actions.js
+++ frontend/app/containers/App/actions.js
@@ -31,12 +31,24 @@ import {
   UNSET_NEW_NOTIFICATIONS_ERROR,
 } from './constants';
 
+/**
+ * Check the new messages, this action starts the request saga
+ *
+ * @return {object} An action object with a type of CHECK_NEW_MESSAGES
+ */
 export function checkNewMessagesAction() {
   return {
     type: CHECK_NEW_MESSAGES,
   };
 }
 
+/**
+ * Dispatched when the new messages are loaded by the request saga
+ *
+ * @param  {array} messages The messages data
+ *
+ * @return {object}      An action object with a type of CHECK_NEW_MESSAGES_SUCCESS passing the repos
+ */
 export function checkNewMessagesSuccessAction(messages) {
   return {
     type: CHECK_NEW_MESSAGES_SUCCESS,
@@ -44,6 +56,13 @@ export function checkNewMessagesSuccessAction(messages) {
   };
 }
 
+/**
+ * Dispatched when loading the new messages fails
+ *
+ * @param  {object} error The error
+ *
+ * @return {object}      An action object with a type of CHECK_NEW_MESSAGES_ERROR passing the repos
+ */
 export function checkNewMessagesErrorAction(error) {
   return {
     type: CHECK_NEW_MESSAGES_ERROR,
@@ -51,12 +70,24 @@ export function checkNewMessagesErrorAction(error) {
   };
 }
 
+/**
+ * Check the new notifications, this action starts the request saga
+ *
+ * @return {object}     An action object with a type of CHECK_NEW_NOTIFICATIONS
+ */
 export function checkNewNotificationsAction() {
   return {
     type: CHECK_NEW_NOTIFICATIONS,
   };
 }
 
+/**
+ * Dispatched when the new notifications are loaded by the request saga
+ *
+ * @param  {number} notificationCount The messages data
+ *
+ * @return {object}      An action object with a type of CHECK_NEW_NOTIFICATIONS_SUCCESS passing the repos
+ */
 export function checkNewNotificationsSuccessAction(notificationCount) {
   return {
     type: CHECK_NEW_NOTIFICATIONS_SUCCESS,
@@ -64,6 +95,13 @@ export function checkNewNotificationsSuccessAction(notificationCount) {
   };
 }
 
+/**
+ * Dispatched when loading the new notifications fails
+ *
+ * @param  {object} error The error
+ *
+ * @return {object}      An action object with a type of CHECK_NEW_NOTIFICATIONS_ERROR passing the repos
+ */
 export function checkNewNotificationsErrorAction(error) {
   return {
     type: CHECK_NEW_NOTIFICATIONS_ERROR,
@@ -71,12 +109,24 @@ export function checkNewNotificationsErrorAction(error) {
   };
 }
 
+/**
+ * Get the new notifications, this action starts the request saga
+ *
+ * @return {object} An action object with a type of GET_NEW_NOTIFICATIONS
+ */
 export function getNewNotificationsAction() {
   return {
     type: GET_NEW_NOTIFICATIONS,
   };
 }
 
+/**
+ * Dispatched when the new notifications are loaded by the request saga
+ *
+ * @param  {array} notifications The notifications data
+ *
+ * @return {object}      An action object with a type of GET_NEW_NOTIFICATIONS_SUCCESS passing the repos
+ */
 export function getNewNotificationsSuccessAction(notifications) {
   return {
     type: GET_NEW_NOTIFICATIONS_SUCCESS,
@@ -84,6 +134,13 @@ export function getNewNotificationsSuccessAction(notifications) {
   };
 }
 
+/**
+ * Dispatched when loading the new notifications fails
+ *
+ * @param  {object} error The error
+ *
+ * @return {object}      An action object with a type of GET_NEW_NOTIFICATIONS_ERROR passing the repos
+ */
 export function getNewNotificationsErrorAction(error) {
   return {
     type: GET_NEW_NOTIFICATIONS_ERROR,
@@ -91,18 +148,35 @@ export function getNewNotificationsErrorAction(error) {
   };
 }
 
+/**
+ * Unset the new notifications, this action starts the request saga
+ *
+ * @return {object} An action object with a type of UNSET_NEW_NOTIFICATIONS
+ */
 export function unsetNewNotificationsAction() {
   return {
     type: UNSET_NEW_NOTIFICATIONS,
   };
 }
 
+/**
+ * Dispatched when unset the new notifications are successed by the request saga
+ *
+ * @return {object}      An action object with a type of UNSET_NEW_NOTIFICATIONS_SUCCESS passing the repos
+ */
 export function unsetNewNotificationsSuccessAction() {
   return {
     type: UNSET_NEW_NOTIFICATIONS_SUCCESS,
   };
 }
 
+/**
+ * Dispatched when unset the new notifications fails
+ *
+ * @param  {object} error The error
+ *
+ * @return {object}      An action object with a type of UNSET_NEW_NOTIFICATIONS_ERROR passing the repos
+ */
 export function unsetNewNotificationsErrorAction(error) {
   return {
     type: UNSET_NEW_NOTIFICATIONS_ERROR,
@@ -110,12 +184,25 @@ export function unsetNewNotificationsErrorAction(error) {
   };
 }
 
+/**
+ * Check user is logged, this action starts the request saga
+ *
+ * @return {object} An action object with a type of IS_LOGGED
+ */
 export function isLoggedAction() {
   return {
     type: IS_LOGGED,
   };
 }
 
+/**
+ * User login to the application, this is the global action
+ *
+ * @param  {number} userId The userId
+ * @param  {string} token The token
+ *
+ * @return {object} An action object with a type of LOGGED_IN
+ */
 export function loggedInAction(userId, token) {
   return {
     type: LOGGED_IN,
@@ -124,46 +211,83 @@ export function loggedInAction(userId, token) {
   };
 }
 
+/**
+ * Start the logout process, this action starts the request saga
+ *
+ * @return {object} An action object with a type of LOGOUT
+ */
 export function logoutAction() {
   return {
     type: LOGOUT,
   };
 }
 
+/**
+ * Dispatched when the logout process are loaded by the request saga
+ *
+ * @return {object} An action object with a type of LOGOUT_SUCCESS
+ */
 export function logoutSuccessAction() {
   return {
     type: LOGOUT_SUCCESS,
   };
 }
 
+/**
+ * Dispatched when loading the new notifications fails
+ *
+ * @param  {object} error The error
+ *
+ * @return {object}      An action object with a type of LOGOUT_ERROR passing the repos
+ */
+export function logoutErrorAction(error) {
+  return {
+    type: LOGOUT_ERROR,
+    error,
+  };
+}
+
+/**
+ * Toggle navigation desktop
+ *
+ * @return {object} An action object with a type of TOGGLE_NAVIGATION_DESKTOP
+ */
 export function toggleNavigationDesktopAction() {
   return {
     type: TOGGLE_NAVIGATION_DESKTOP,
   };
 }
 
+/**
+ * Toggle navigation mobile
+ *
+ * @return {object} An action object with a type of TOGGLE_NAVIGATION_MOBILE
+ */
 export function toggleNavigationMobileAction() {
   return {
     type: TOGGLE_NAVIGATION_MOBILE,
   };
 }
 
+/**
+ * Toggle messages
+ *
+ * @return {object} An action object with a type of TOGGLE_MESSAGES
+ */
 export function toggleMessagesAction() {
   return {
     type: TOGGLE_MESSAGES,
   };
 }
 
+/**
+ * Toggle notifications
+ *
+ * @return {object} An action object with a type of TOGGLE_NOTIFICATIONS
+ */
 export function toggleNotificationsAction() {
   return {
     type: TOGGLE_NOTIFICATIONS,
-  };
-}
-
-export function logoutErrorAction(error) {
-  return {
-    type: LOGOUT_ERROR,
-    error,
   };
 }
 
@@ -179,6 +303,7 @@ export function enqueueSnackbarAction(snackbar) {
   };
 }
 
+// todo: check if this action is use
 export function closeSnackbarAction(key) {
   return {
     type: CLOSE_SNACKBAR,
@@ -187,6 +312,11 @@ export function closeSnackbarAction(key) {
   };
 }
 
+/**
+ * Remove snackbars from the screen
+ *
+ * @return {object} An action object with a type of REMOVE_SNACKBAR
+ */
 export function removeSnackbarAction() {
   return {
     type: REMOVE_SNACKBAR,
