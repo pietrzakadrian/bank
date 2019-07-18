6db62ab1b323c5193d7f47e506c7b5aa34b4ca08
PietrzakAdrian <contact.pietrzakadrian@gmail.com>
2019-07-18 23:18:21 +0200

    make a Users, Login, Logout tests

--- backend/src/tests/tests-helper.ts
+++ backend/src/tests/tests-helper.ts
@@ -4,6 +4,7 @@ import chai from "chai";
 import chaiHttp = require("chai-http");
 import { before } from "mocha";
 import "mocha";
+import config from "../config/config";
 
 chai.use(chaiHttp);
 const should = chai.should();
@@ -17,5 +18,13 @@ before(async () => {
   application = await new Application();
   await application.setupDbAndServer();
   app = application.app;
+
+  console.log("## Authenticating to get token...");
+  let response = await chai
+    .request(app)
+    .post("/api/auth/login")
+    .send({ login: config.admin.login, password: `${config.admin.password}` });
+  authToken = "Bearer " + response.body.token;
+
   console.info("## Starting tests...");
 });
---
+++ backend/src/tests/users.spec.ts
@@ -0,0 +1,97 @@
+import "mocha";
+
+import chai from "chai";
+import chaiHttp = require("chai-http");
+
+chai.use(chaiHttp);
+const should = chai.should();
+import { app, authToken } from "./tests-helper";
+import config from "../config/config";
+
+describe("Users", () => {
+  describe("GET /api/users", () => {
+    it("should be able to return basic user's data", done => {
+      chai
+        .request(app)
+        .get("/api/users")
+        .set("Authorization", authToken)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("name");
+          res.body.should.have.property("surname");
+          res.body.should.have.property("email");
+          res.body.should.have.property("lastSuccessfulLoggedDate");
+          res.body.should.have.property("lastPresentLoggedDate");
+          res.body.should.have.property("lastFailedLoggedDate");
+          done();
+        });
+    });
+
+    it("should not able to return basic user's data without token", done => {
+      chai
+        .request(app)
+        .get("/api/users")
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.success.should.equal(false);
+          done();
+        });
+    });
+  });
+
+  describe("PATCH /api/users", () => {
+    it("should be able to updates basic user's data", done => {
+      chai
+        .request(app)
+        .patch("/api/users")
+        .set("Authorization", authToken)
+        .send({ currencyId: 2 })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success");
+          res.body.success.should.equal(true);
+          done();
+        });
+    });
+
+    it("should not able to updates basic user's data without token", done => {
+      chai
+        .request(app)
+        .patch("/api/users")
+        .send({ currencyId: 3 })
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.success.should.equal(false);
+          done();
+        });
+    });
+  });
+
+  describe("GET /api/users/{login}/isLogin", () => {
+    it("should be able to return boolean that login exists", done => {
+      chai
+        .request(app)
+        .get(`/api/users/${config.admin.login}/isLogin`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.isLogin.should.equal(true);
+          done();
+        });
+    });
+  });
+
+  describe("GET /api/users/{email}/isEmail", () => {
+    it("should be able to return boolean that email exists", done => {
+      chai
+        .request(app)
+        .get(`/api/users/${config.admin.email}/isEmail`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.isEmail.should.equal(true);
+          done();
+        });
+    });
+  });
+});
