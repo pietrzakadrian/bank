0ea6705fd9feda9d95d312620436ef20bead71cc
api <api@crehler.com>
2019-07-26 15:36:21 +0200

    cleancode

--- frontend/app/components/App/Header/WidgetContainer.js
+++ frontend/app/components/App/Header/WidgetContainer.js
@@ -5,9 +5,16 @@
  */
 
 import styled from 'styled-components';
+import { PHONE_LANDSCAPE_VIEWPORT_WIDTH } from 'utils/rwd';
 
 const WidgetContainer = styled.div`
   display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+
+  @media screen and (min-width: ${PHONE_LANDSCAPE_VIEWPORT_WIDTH}) {
+    width: auto;
+  }
 `;
 
 export default WidgetContainer;
